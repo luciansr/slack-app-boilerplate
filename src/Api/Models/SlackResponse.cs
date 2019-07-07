eb1c5d7306001a26d14469c14c71bf398b20fc2d
Lucian Sturião <luciansr@gmail.com>
2019-07-07 11:59:12 -0300

    Change json property

--- src/Api/Models/SlackResponse.cs
+++ src/Api/Models/SlackResponse.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace Api.Models
 {
     public class SlackResponse
     {
+        [JsonProperty("response_type")]
         public SlackResponseType ResponseType { get; set; }
         public string Text { get; set; }
         public List<SlackAttachment> Attachments { get; set; }
