6747537a072e27358948439e81bea9a14a9cb99f
Lucian Sturião <luciansr@gmail.com>
2020-07-19 15:30:37 -0300

    Add Action executor factory tests

---
+++ src/Tests/Services/Events/Processors/ActionExecutorFactoryTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Models.Events;
+using Services.Events.Actions;
+using Services.Events.Matchers;
+using Services.Events.Processors;
+using Xunit;
+
+namespace Tests.Services.Events.Processors
+{
+    public class ActionExecutorFactoryTests
+    {
+        [Theory]
+        [InlineData(ActionType.AnswerToMessage, typeof(AnswerMessageActionExecutor))]
+        public void ProviderCreatesCorrectMatchers(ActionType actionType, Type eventMatcherType)
+        {
+            var actionExecutorFactory = new ActionExecutorFactory();
+
+            var actionExecutor = actionExecutorFactory.GetActionExecutor(
+                new ActionConfiguration
+                {
+                    Type = actionType
+                });
+            
+            Assert.Equal(eventMatcherType, actionExecutor.GetType());
+        }
+    }
+}
